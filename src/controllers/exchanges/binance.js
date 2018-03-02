9a6c1fe14472f9805719633d82f35d6fa07eb654
ttbarnes <tony@tonybarnes.me>
2018-03-02 16:30:54 +0000

    binance trades

--- src/controllers/exchanges/binance.js
+++ src/controllers/exchanges/binance.js
@@ -23,6 +23,30 @@ export const checkBinanceKeys = (exchange: Object): Promise<Object> => {
   });
 };
 
+const getBinanceAccountBalanceSymbols = (): Promise<Array<string>> => {
+  const assetSymbolsWithBalance = [];
+  return new Promise((resolve: any, reject: any): Promise<Array<string>> =>
+    
+    binance.account((err: Object, account: Object, symbol: string): Promise<Function> => {
+      account.balances.map((asset: Object): Object => {
+        if (asset.free > 0) {
+          assetSymbolsWithBalance.push(asset.asset);
+        }
+        return asset;
+      });
+      return resolve(assetSymbolsWithBalance);
+    })
+  );
+};
+
+const getBinanceTradesFromSymbol = (symbol: string): Promise<Array<Object>> => {
+  return new Promise((resolve: any, reject: any): Promise<Array<Object>> => {
+    return binance.trades(symbol, (err: Object, data: Array<Object>, symbol: string): Promise<Array<Object>> =>
+      resolve(data)
+    );
+  });
+};
+
 export const getBinanceTradeHistory = (exchange: Object): Object => {
   const decryptedObj = getKeys(exchange);
 
@@ -33,12 +57,34 @@ export const getBinanceTradeHistory = (exchange: Object): Object => {
   });
 
   return new Promise((resolve: any, reject: any): Promise<Object> => {
-    return binance.trades('REQBTC', (err: Object, data: Array<Object>, symbol: string): Promise<Function> => {
-      
-      if (err) return reject({ binanceError: true });
-      return resolve(
-        formatResponse(exchange.name, data)
-      );
+    // currently with binance, doesn't seem possible to get a list of symbols/markets for a user
+    // for V1 we can get trades purely from accounts that have balances
+    // for now we also hard code into BTC pair. Need to have an approach/solution that will account for BTC, ETH, others.
+
+    const binanceTrades = [];
+
+    const onComplete = (): Promise<Object> => resolve(
+      formatResponse(exchange.name, binanceTrades)
+    );
+
+    return getBinanceAccountBalanceSymbols().then((balanceSymbols: Array<string>): any => {
+      let balanceSymbolsCount = balanceSymbols.length;
+
+      if (balanceSymbolsCount === 0) {
+        return onComplete();
+      } else {
+        balanceSymbols.forEach((symbol: string): any => {
+          getBinanceTradesFromSymbol(`${symbol}BTC`).then((results: any): any => {
+            binanceTrades.push(...results);
+            if (--balanceSymbolsCount === 0) {
+              return onComplete();
+            }
+          });
+        });
+      }
+    }, (err: any): Promise<Object> => {
+      return reject({ error: err });
     });
+
   });
 };
--- src/controllers/exchanges/utils.js
+++ src/controllers/exchanges/utils.js
@@ -1,6 +1,7 @@
 // @flow
 
-export const formatResponse = (name: string, data: Array<Object>): Object => {
+// export const formatResponse = (name: string, data: Array<Object>): Object => {
+  export const formatResponse = (name: string, data: any): Object => {
   return {
     name,
     data
